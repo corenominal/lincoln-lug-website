4b854fa2fe71744f3269580ab70e1bdce80fbd85
Philip Newborough <corenominal@corenominal.org>
2016-01-07 18:13:53 +0000

    Initial import

---
+++ public_html/js/lincolnlug.js
@@ -0,0 +1,21 @@
+$(document).ready(function()
+{
+    /*
+    If SVG not supported, replace svg images with png
+    */
+    if(!Modernizr.svg)
+    {
+	    $('img[src*="svg"]').attr('src', function()
+	    {
+	        return $(this).attr('src').replace('.svg', '.png');
+	    });
+	    var logo_img_ext = 'png'
+	}
+	else
+	{
+		var logo_img_ext = 'svg'
+	}
+
+	$('body').snowfall({flakeCount : 25, image :"http://lincoln.lug.org.uk/img/christmas-snowflake.png", minSize: 10, maxSize:32});
+
+});
